8b1dcfafb18b691f8cd293650ae5ec8ece997aa2
Khangb1910527 <khangb1910527@student.ctu.edu.vn>
2022-10-23 20:04:46 +0700

    Xay dung trang hieu chinh san pham

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:myshop/ui/products/edit_product_screen.dart';
 import 'ui/products/product_detail_screen.dart';
 import 'ui/products/products_manager.dart';
-import 'ui/products/product_overview_screen.dart';
+import 'ui/products/products_overview_screen.dart';
 import 'ui/products/user_products_screen.dart';
 import 'ui/products/user_product_list_tile.dart';
 import 'ui/oders/orders_screen.dart';
--- lib/ui/screens.dart
+++ lib/ui/screens.dart
@@ -1,4 +1,4 @@
-export 'products/product_overview_screen.dart';
+export 'products/products_overview_screen.dart';
 export 'products/product_detail_screen.dart';
 export 'products/user_products_screen.dart';
 export 'products/products_manager.dart';
